9f0370631b86950a6b2ad90a6196de11d7326e3b
michalBezdek <145441056+michalBezdek@users.noreply.github.com>
2024-02-26 12:21:20 +0100

    Add files via upload

---
+++ morzeovka/script.js
@@ -0,0 +1,32 @@
+let pismena = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    ' ': ' '
+};
+
+function konvertovat() {
+    let inputText = document.getElementById("pismena").value.toUpperCase();
+    let result = [...inputText].map(char => pismena[char] || ' ').join(' ');
+    document.getElementById("pep").innerHTML = result;
+}
+
+let morzeokvka = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
+    '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P',
+    '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+    '-.--': 'Y', '--..': 'Z',
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
+    '-....': '6', '--...': '7', '---..': '8', '----.': '9',
+    ' ': ' '
+};
+
+function konvertovat2() {
+    let inputText = document.getElementById("morzeovka").value.toUpperCase();
+    let morseSymbols = inputText.split(' ');  
+    let result = morseSymbols.map(symbol => morzeokvka[symbol] || ' ').join('');
+    document.getElementById("pop").innerHTML = result;
+}
